e8ae8fa00b64e2d7ee607a03a767014c9b98f58e
Dylan Pike <107321662+Dtpike02@users.noreply.github.com>
2025-07-24 09:55:37 -0400

    Fix undefined router in Header

--- garden-ai/src/components/Header.jsx
+++ garden-ai/src/components/Header.jsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   const handleSignIn = () => {
     // Standard sign-in, redirect to /ask on success
